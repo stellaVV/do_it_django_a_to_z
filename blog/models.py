f821a8def9fcc95fdce92004b637ed15e8c29ce9
HyunJin Kim <guswlsdl0109@naver.com>
2023-01-06 22:48:46 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,9 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    created_at = models.DateTimeField()
+    head_image = models.ImageField(upload_to='bolg/images/%Y/%m/%d/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
